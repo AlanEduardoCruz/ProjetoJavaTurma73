4b54e93b8b37e82e39130a1c3371dac8a5bbe8a2
Alan <alan-hd@hotmail.com>
2024-02-28 16:16:31 -0300

    Realizado exercicio-06

---
+++ src/laco_repeticao/Exemplo6.java
@@ -0,0 +1,38 @@
+package laco_repeticao;
+
+import java.util.Scanner;
+
+public class Exemplo6 {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+
+		// Variaveis
+
+		int numero;
+		float media = 0.0f;
+		float soma = 0.0f;
+		int contador = 0;
+
+		do {
+			System.out.println("Digite um número:");
+			numero = leia.nextInt();
+
+			if (numero != 0 && numero % 3 == 0) {
+				soma += numero;
+				contador++;
+			}
+
+		} while (numero != 0);
+
+		if (contador != 0) {
+			media = soma / contador;
+		}
+
+		
+		System.out.println("A média de todos os números múltiplos de 3 é: " + media);
+		System.out.println("Fim do loop");
+		leia.close();
+	}
+
+}
