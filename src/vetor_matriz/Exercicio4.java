7434d4b5a561c6df70695828defb84d3e17e8c24
Alan <alan-hd@hotmail.com>
2024-02-29 18:57:44 -0300

    Realizado exercicio-06

--- src/vetor_matriz/Exercicio4.java
+++ src/vetor_matriz/Exercicio4.java
@@ -1,34 +1,35 @@
 package vetor_matriz;
 
-
 import java.util.Scanner;
 
 public class Exercicio4 {
 
 	public static void main(String[] args) {
-		Scanner leia = new Scanner (System.in);
-		
-			
-		int linha, coluna;		
-		float matriz [][] = new float [3][3];
-		float somaLinha =0.f;
-		
-		for(linha = 0; linha < 3; linha++) {
-			for(coluna = 0; coluna < 3; coluna++) {
-				System.out.println("Digite o numéro na posiçao " + "[" + linha + "]" + "[" + coluna + "]");
-				matriz[linha][coluna] = leia.nextFloat();
+		Scanner leia = new Scanner(System.in);
+		leia.close();
+
+		int linha, coluna;
+		float[][] matriz = { { 4.0f, 5.0f, 7.0f, 3.0f }, { 2.5f, 6.5f, 4.7f, 8.0f }, { 10.0f, 8.5f, 9.5f, 8.0f },
+				{ 9.0f, 6.5f, 7.6f, 8.2f }, { 5.0f, 5.0f, 5.0f, 6.3f }, { 7.0f, 8.0f, 9.0f, 8.5f },
+				{ 5.5f, 3.5f, 2.5f, 1.0f }, { 8.0f, 9.0f, 10.0f, 9.5f }, { 5.6f, 5.8f, 6.5f, 7.0f },
+				{ 7.5f, 8.5f, 9.5f, 10.0f } };
+		float[] mediaLinhas = new float[matriz.length];
+
+		for (linha = 0; linha < matriz.length; linha++) {
+
+			float somaLinha = 0f;
+
+			for (coluna = 0; coluna < matriz[linha].length; coluna++) {
+//				
+				somaLinha += (matriz[linha][coluna]);
 			}
+			mediaLinhas[linha] = somaLinha / matriz[linha].length;
 		}
-		for(linha = 0; linha < 3; linha++) {
-			
-			for(coluna = 0; coluna < 3; coluna++) {
-			 somaLinha += (matriz[linha][coluna]);			
-			}
+		System.out.println("Vetor:");
+		for (linha = 0; linha < matriz.length; linha++) {
+
+			System.out.printf("%.1f ",mediaLinhas[linha]);
 		}
-		
-		
-		
-		
 
 	}
 
